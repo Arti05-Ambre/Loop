28fd1868cfee8b4839809dcf3fba138b20d25408
Arti05-Ambre <ambrearti62@gmail.com>
2026-08-18 22:46:14 +0530

    Remove duplicate print statement in Nested.java

--- Nested.java
+++ Nested.java
@@ -6,7 +6,7 @@ public class Nested {
 		for(int i = 1; i<=3; i++) {
 			System.out.println("table of"+i);
 			for (int j =1; j<=10; j++) { 
-				System.out.println(i+" "+j+"=" +(i*j));
+				System.out.println(i+" "+j+"=" +(i*j)); 
 			}
 			
 		}
